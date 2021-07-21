3a9921fdbaa0399cc8b9c5767b9249988028bf4f
KyuSahm Kim <gusami32@gmail.com>
2021-07-21 23:13:25 +0900

    Current Status Save with Servlet Context(Application repository)

--- src/main/java/com/newlecture/web/Calc2.java
+++ src/main/java/com/newlecture/web/Calc2.java
@@ -3,6 +3,7 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;        
 import javax.servlet.http.HttpServlet;
@@ -24,35 +25,47 @@ public class Calc2 extends HttpServlet
 		response.setCharacterEncoding("UTF-8");		
 		// 웹브라우저가 읽을 때 사용하는 인코딩을 정해줌(브라우저가 받은 문자열을 어떻게 해석할지를 결정)		
         response.setContentType("text/html;charset=UTF-8");		
-		        
-		String var1 = request.getParameter("var1");
-        String var2 = request.getParameter("var2");
+        
+        String var = request.getParameter("var");
         String operator = request.getParameter("operator");
         
-        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter();        
         
-        // html에서 var1이 존재하면 null값은 오지 않음.
-        if (!var1.equals("") && !var2.equals(""))
+        int a = 0;
+        
+        // html에서 var이 존재하면 null값은 오지 않음.
+        if (!var.equals(""))
         {
-        	int a = Integer.parseInt(var1);
-        	int b = Integer.parseInt(var2);
+        	a = Integer.parseInt(var);
+        }
+        
+        ServletContext application = request.getServletContext();
+        if (operator.equals("="))
+        {
+        	// Servlet Context에서 저장된 값을 Attribute 이름을 통해 찾아옴.
+        	String op = (String)application.getAttribute("op");
+        	int x = (Integer)application.getAttribute("value");        	
+        	int y = a;
         	
-        	if (operator.equals("덧셈"))
+        	if (op.equals("+"))
         	{
-	        	int result = a + b;        	
+	        	int result = x + y;        	
 	        	out.println("덧셈 결과:");
-	            out.printf("%d + %d = %d",  a, b, result);
+	            out.printf("%d + %d = %d",  x, y, result);
         	}
         	else
         	{
-        		int result = a - b;        	
+        		int result = x - y;        	
 	        	out.println("뺄셈 결과:");
-	            out.printf("%d - %d = %d",  a, b, result);
+	            out.printf("%d - %d = %d",  x, y, result);
         	}
         }
         else
         {
-        	out.println("연산을 수행할 수 없습니다");
+        	// 향후에 사용될 것을 대비해 Servlet Context에 저장함.
+            // Attribute "이름문자열"에 Object형태로 저장한다.            
+            application.setAttribute("value",  a);
+            application.setAttribute("op",  operator);            
         }
     }
 }
