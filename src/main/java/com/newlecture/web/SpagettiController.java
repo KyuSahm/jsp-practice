828f73b06313f0f994b62081e541cd82a167e717
KyuSahm <gusami32@gmail.com>
2021-08-04 23:51:45 +0900

    From MVC Model 1 to MVC Model 2 before dispatcher concentration

---
+++ src/main/java/com/newlecture/web/SpagettiController.java
@@ -0,0 +1,45 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/spagetti")
+public class SpagettiController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		int num = 0;
+		String numStr = request.getParameter("num");		
+		
+		if (numStr != null && !numStr.equals(""))
+		{
+		    num = Integer.parseInt(numStr);
+		}
+		
+		String model;
+		
+		if (num % 2 != 0) {
+			model = "홀수";
+		}
+		else
+		{
+			model = "짝수";
+		}
+		
+		// Step 1: request에 값을 담음.
+		request.setAttribute("model", model);
+		
+		// redirect or forward
+		// redirect => 현재 작업한 내용과 상관없이 다른 페이지로 이동
+		// forward => 현재 작업한 내용을 이어갈수 있도록 모델을 공유
+		// Step 2: Dispatcher를 이용해서 "spagetti.jsp"에 Forward함 
+		RequestDispatcher dispatcher = request.getRequestDispatcher("spagetti.jsp");
+		dispatcher.forward(request,  response);
+	}
+}
--- src/main/webapp/spagetti.jsp
+++ src/main/webapp/spagetti.jsp
@@ -1,28 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!-- Controller of MVC1 -->    
-<%   
-    int num = 0;
-	String numStr = request.getParameter("num");
-	
-	
-	if (numStr != null && !numStr.equals(""))
-	{
-	    num = Integer.parseInt(numStr);
-	}
-	
-	String model;
-	
-	if (num % 2 != 0) {
-		model = "홀수";
-	}
-	else
-	{
-		model = "짝수";
-	}	
-%>
-
-<!-- View of MVC1 -->
+<!-- View of MVC2 -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,7 +8,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-    <!-- Model of MVC1 --> 
-    <%=model%>입니다.
+    <%=request.getAttribute("model")%>입니다.
 </body>
 </html>
